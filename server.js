78b6a179c1b0d84b23965cf17888be2233f1f373
unknown <you@example.com>
2020-05-27 15:46:33 +0100

    added

--- server.js
+++ server.js
@@ -13,51 +13,11 @@ app.use(express.static(__dirname + '/'));
 
 
 app.get('/', function (req, res){  
-    var date = new Date();
-    var Day = date.getDay();
-    var year = date.getFullYear();
-    var month = date.getMonth();
-    var dates = date.getDate();
-    var Today = "";
-    var fulldata = "";
-
-        switch(Day){
-            case 3:
-                Today = "Wednesday"; 
-            break;
-            case 4: 
-            Today = "Thursday";
-            break;
-            case 5: 
-            Today = "Friday";
-            break;
-            case 6: 
-            Today = "Saturday";
-            break;
-            case 0: 
-            Today = "Sunday";
-            break;
-            case 1: 
-            Today = "Monday";
-            break;
-            case 2: 
-            Today = "Tuesday";
-            break;
-            default: console.log("error");
-        }
-
-        fulldata = Today + " -   " + dates + " / " + month + " /   " + year;
-    
-    res.render('index', {dateinfo: fulldata, listofitems: items});
+res.render('index');
 });
 
 
-app.post('/', function(req, res){
-    var item = req.body.newItem;
-    items.push(item);
-    res.redirect('/');
 
-})
 
 app.listen(port, function(req, res){
     console.log("Server is up and Running on port " + port );
